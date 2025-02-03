14092a1b5f28319da58dcd372c108e6505bafb38
Felipe Navarrete <felipe.hnn@gmail.com>
2025-02-03 12:39:20 +0100

    minor fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 case "$1" in
   1)
-    mpirun -np 4 ./simulation_model.bin -np 1 python evaluation_software.py
+    mpirun -np 4 ./simulation_model.bin : -np 1 python evaluation_software.py
     ;;
   2)
     mpirun -np 4 ./simulation_model_2.bin : -np 1 python evaluation_software.py
